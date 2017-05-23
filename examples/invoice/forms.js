3db654207e5e2d1e4a6517aca13a19a245c5ca53
Alexey Sergeev <sab@scand.com>
2017-05-24 01:54:03 +0300

    Adding possibility to (re)start machine

--- examples/invoice/forms.js
+++ examples/invoice/forms.js
@@ -1,12 +1,13 @@
 const blessed = require("blessed");
 
-const createForm = ({
+const eventForm = ({
   screen,
   formProperties = {},
   availableEvents = [],
   onSubmit = () => {}
 }) => {
   const form = blessed.form({
+    label: 'Action Form',
     mouse: true,
     keys: true,
     vi: true,
@@ -17,6 +18,7 @@ const createForm = ({
     scrollbar: {
       ch: " "
     },
+    border: 'line',
     ...formProperties
   });
 
@@ -40,6 +42,7 @@ const createForm = ({
     options.push(blessed.radiobutton({
       parent: radioset,
       mouse: true,
+      checked: (i === 0),
       keys: true,
       shrink: true,
       top: top++,
@@ -53,10 +56,10 @@ const createForm = ({
     parent: form,
     mouse: true,
     keys: true,
-    top: 10,
+    top: top++,
     shrink: true,
     name: "submit",
-    content: "submit",
+    content: "Send Event",
     padding: {
       left: 1,
       right: 1
@@ -82,4 +85,68 @@ const createForm = ({
   return form;
 };
 
-export default createForm;
+const startForm = ({
+  screen,
+  formProperties = {},
+  submitLabel = 'Start',
+  onSubmit = () => {},
+}) => {
+  // console.log(`arguments[0]: '${JSON.stringify(arguments[0])}'`);
+  const form = blessed.form({
+    label: 'Action Form',
+    mouse: true,
+    keys: true,
+    vi: true,
+    left: 0,
+    top: 0,
+    width: "100%",
+    scrollable: true,
+    scrollbar: {
+      ch: " "
+    },
+    border: 'line',
+    ...formProperties
+  });
+
+  form.on("submit", onSubmit);
+
+  const submit = blessed.button({
+    parent: form,
+    mouse: true,
+    keys: true,
+    // top: 10,
+    shrink: true,
+    name: "submit",
+    content: submitLabel,
+    padding: {
+      left: 1,
+      right: 1
+    },
+    style: {
+      bg: "white",
+      fg: "black",
+      focus: {
+        bg: "blue"
+      }
+    }
+  });
+
+  submit.on("press", function() {
+    form.submit();
+  });
+
+  submit.focus();
+  screen.render();
+
+  return form;
+};
+
+const restartForm = (input) => {
+  return startForm({...input, submitLabel:'Start'});
+}
+
+export default {
+  startForm,
+  eventForm,
+  restartForm,
+};
--- examples/invoice/index.js
+++ examples/invoice/index.js
@@ -1,31 +1,31 @@
 let fs = require("fs");
 
-let createForm = require('./index-form').default;
+let { startForm, eventForm, restartForm } = require("./forms").default;
 
-let MachineDefinition = require('../../src/MachineDefinition').default;
-let Machine = require('../../src/Machine').default;
+let MachineDefinition = require("../../src/MachineDefinition").default;
+let Machine = require("../../src/Machine").default;
 
-let machineSchemaAsText = fs.readFileSync('./machine-schema.json', 'utf8')
+let machineSchemaAsText = fs.readFileSync("./machine-schema.json", "utf8");
 
 let machineDefinition = new MachineDefinition({
   schema: JSON.parse(machineSchemaAsText)
 });
 
-let invoiceMachine = new Machine({machineDefinition});
+let invoiceMachine = new Machine({ machineDefinition });
 
 let invoice = {
-  status: 'none'
+  status: "none"
 };
 
-let blessed = require('blessed');
-let program = blessed.program()
+let blessed = require("blessed");
+let program = blessed.program();
 
 // Create a screen object.
 let screen = blessed.screen({
   smartCSR: true,
   cursor: {
-    shape: 'line',
-    blink: true,
+    shape: "line",
+    blink: true
   }
 });
 
@@ -34,134 +34,178 @@ let titleBox = blessed.box({
   parent: screen,
   top: 0,
   left: 0,
-  width: '100%',
+  width: "100%",
   height: 3,
-  content: ' Invoice machine',
+  content: " Invoice machine",
   border: {
-    type: 'line'
+    type: "line"
   }
 });
 
-// let availableEvents = [];
-
-const logMessage = (text) => {
-  let content = outputBox.getContent();
-  content = text + '\n' + content.trim();
-  outputBox.setContent(content);
+const logMessage = text => {
+  outputBox.log(text);
   screen.render();
-}
-const updateObjectContent = (object) => {
-  objectBox.setContent(JSON.stringify(invoice, null, '\t'));
-  screen.render();
-}
+};
 
-const handleFormSubmit = ({event}) => {
-  // console.log('opa');
-  logMessage(`Sending event '${event} ...'\n`);
-  invoiceMachine.sendEvent({
-    object: invoice,
-    event,
-  }).then(({object}) => {
-    invoice = object;
-    logMessage("invoice machine has been started...");
-    updateObjectContent({object});
-    logMessage("finding events that are available...");
-    return invoiceMachine.availableTransitions({object})
-  }).then(({transitions}  ) => {
-    const availableEvents = transitions.map((transition) => {
-      return transition.event
-    });
-    logMessage(`available events '${availableEvents}'`);
-    recreateForm({availableEvents});
-  }).catch(({message}) => {
-    logMessage(`error '${message}'`);
+const updateObjectContent = object => {
+  objectBox.setContent(JSON.stringify(invoice, null, "\t"));
+  screen.render();
+};
+
+const startOrRestartForm = objectWithF => {
+  objectWithF["f"]({
+    screen,
+    formProperties: {
+      parent: screen,
+      left: 0,
+      top: 3,
+      width: "50%-1",
+      height: "50%-3"
+    },
+    onSubmit: () => {
+      logMessage("starting invoice machine...");
+      invoiceMachine.
+        start({
+          object: invoice
+        }).
+        then(({ object }) => {
+          invoice = object;
+          logMessage("invoice machine has been started");
+          updateObjectContent({ object });
+          if (invoiceMachine.isFinal({state: invoiceMachine.currentState({ object })})) {
+            logMessage("current state is final, machine is stopped!");
+            formBox = startOrRestartForm({ f: restartForm });
+            return;
+          } else {
+            logMessage("finding events that are available...");
+            return invoiceMachine.availableTransitions({ object }).then(({ transitions }) => {
+              const availableEvents = transitions.map(transition => {
+                return transition.event;
+              });
+              logMessage(`available events: '${availableEvents}'`);
+              rerenderEventForm({ availableEvents });
+            });
+          }
+        }).
+        catch(({ message }) => {
+          logMessage(`error message: '${message}'`);
+        });
+    }
   });
-}
+};
 
-const recreateForm = ({availableEvents}) => {
-  return createForm({
+const handleFormSubmit = ({ event }) => {
+  logMessage(`sending event: '${event}'...`);
+  invoiceMachine.
+    sendEvent({
+      object: invoice,
+      event
+    }).
+    then(({ object }) => {
+      invoice = object;
+      logMessage("transition just finished");
+      updateObjectContent({ object });
+      if (invoiceMachine.isFinal({state: invoiceMachine.currentState({ object })})) {
+        logMessage("current state is final, machine is stopped!");
+        formBox = startOrRestartForm({ f: restartForm });
+        return;
+      } else {
+        logMessage("finding events that are available...");
+        return invoiceMachine.availableTransitions({ object }).then(({ transitions }) => {
+          const availableEvents = transitions.map(transition => {
+            return transition.event;
+          });
+          logMessage(`available events: '${availableEvents}'`);
+          rerenderEventForm({ availableEvents });
+        });
+      }
+    }).
+    catch(({ message }) => {
+      logMessage(`error message: '${message}'`);
+    });
+};
+
+const rerenderEventForm = ({ availableEvents }) => {
+  return eventForm({
     screen,
     availableEvents,
     formProperties: {
       parent: screen,
       left: 0,
       top: 3,
-      width: '50%-1',
-      height: '50%-3',
-      border: 'line'
+      width: "50%-1",
+      height: "50%-3"
     },
     onSubmit: handleFormSubmit
   });
 };
 
-let formBox = recreateForm({availableEvents: []})
+// let formBox = rerenderEventForm({ availableEvents: [] });
+let formBox = startOrRestartForm({ f: startForm });
 
-let outputBox = blessed.scrollabletext({
+let outputBox = blessed.log({
+  label: "Log",
   parent: screen,
   mouse: true,
   keys: true,
   left: 0,
-  top: '50%',
-  width: '50%-1',
-  height: '50%',
-  border: 'line',
-  content: ""
+  top: "50%",
+  width: "50%-1",
+  height: "50%",
+  border: "line",
+  scrollback: 100,
+  scrollbar: {
+    ch: " ",
+    track: {
+      bg: "blue"
+    },
+    style: {
+      inverse: true
+    }
+  }
 });
 
 let objectBox = blessed.box({
+  label: "Invoice",
   parent: screen,
-  left: '50%+1',
+  left: "50%+1",
   top: 3,
-  width: '50%-1',
-  height: '30%-3',
-  border: 'line',
-  content: JSON.stringify(invoice, null, '\t')
-});
-
-let fsmSchemaBox = blessed.box({
-  parent: screen,
-  left: '50%+1',
-  top: '30%',
-  width: '50%-1',
-  height: '70%',
-  border: 'line',
-  keys: true,
-  vi: true,
+  width: "50%-1",
+  height: "25%-3",
+  border: "line",
+  content: JSON.stringify(invoice, null, "\t"),
   scrollable: true,
   alwaysScroll: true,
   scrollbar: {
-    bg: 'blue'
+    bg: "blue"
+  }
+});
+
+let fsmSchemaBox = blessed.box({
+  label: "Machine Schema",
+  parent: screen,
+  left: "50%+1",
+  top: "25%",
+  width: "50%-1",
+  height: "75%",
+  border: "line",
+  keys: true,
+  scrollable: true,
+  alwaysScroll: true,
+  scrollbar: {
+    bg: "blue"
   },
   scrollbar: {
-    ch: ' ',
+    ch: " ",
     inverse: true
   },
   content: machineSchemaAsText
 });
 
 // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+screen.key(["escape", "q", "C-c"], function(ch, key) {
   return process.exit(0);
 });
 
 // Render the screen.
 screen.render();
-
-logMessage("starting invoice machine...")
-invoiceMachine.start({
-  object: invoice
-}).then(({object}) => {
-  invoice = object;
-  logMessage("invoice machine has been started...");
-  updateObjectContent({object});
-  logMessage("finding events that are available...");
-  return invoiceMachine.availableTransitions({object});
-}).then(({transitions}  ) => {
-  const availableEvents = transitions.map((transition) => {
-    return transition.event
-  });
-  logMessage(`available events '${availableEvents}'`);
-  recreateForm({availableEvents});
-}).catch(({message}) => {
-  logMessage(`error '${message}'`);
-});
