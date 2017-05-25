440da091797d0d4afc7c0c7f33fa3d8b91229fc6
Alexey Sergeev <sab@scand.com>
2017-05-25 10:28:02 +0300

    Restructuring sources

--- obsolete/20170519/WorkflowDefinitionProvider.js
+++
@@ -1,8 +0,0 @@
-export default class WorkflowDefinitionProvider {
-  constructor() {
-    this.definitions = {};
-  }
-  register({definition, guards, actions}) {
-    this.definitions[schema.name] = new WorkflowDefinition({ schema, guards, actions });
-  }
-}
