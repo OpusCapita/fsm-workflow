a188472a930dc571d6ad47915c81337b650a8293
Egor Stambakio <stambakio@scand.com>
2018-01-20 14:09:06 +0300

    ...

--- scripts/check-commit.sh
+++ scripts/check-commit.sh
@@ -14,6 +14,18 @@ CRUD_EDITOR_COMMIT=$(git log -1 --format=format:%H --full-diff crud-editor)
 # latest commit where task-manager was changed
 TASK_MANAGER_COMMIT=$(git log -1 --format=format:%H --full-diff task-manager)
 
+REPO="fsm-workflow"
+CIRCLE_CI_TOKEN="whatever"
+
+# replace "/", "#", etc. in current git branch name
+urlencode() {
+  node -e "console.log('${*}'.replace('/', '%2F').replace('#', '%23'))"
+}
+
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+SAFE_GIT_BRANCH=`urlencode $GIT_BRANCH`
+echo "Current branch is $SAFE_GIT_BRANCH"
+
 if [ $CORE_COMMIT = $LATEST_COMMIT ]
   then
     echo "files in core has changed"
@@ -23,7 +35,8 @@ fi
 if [ $CRUD_EDITOR_COMMIT = $LATEST_COMMIT ]
   then
     echo "files in crud-editor has changed"
-    # run script
+    JOB_URL=`curl -s -X POST -d build_parameters[CIRCLE_JOB]=$3 https://circleci.com/api/v1.1/project/github/opuscapita/${REPO}/tree/${SAFE_GIT_BRANCH}?circle-token=${CIRCLE_CI_TOKEN} | grep build_url | perl -pe 's/^.*(?=https)//g' | perl -pe 's/".*$//g'`
+    echo $JOB_URL
 fi
 
 if [ $TASK_MANAGER_COMMIT = $LATEST_COMMIT ]
@@ -31,3 +44,5 @@ if [ $TASK_MANAGER_COMMIT = $LATEST_COMMIT ]
     echo "files in task-manager has changed"
     # run script
 fi
+
+echo "check-commit is done."
