53a13f83172560bb1502989148da160c3f61dab3
Egor Stambakio <stambakio@scand.com>
2018-01-20 14:12:24 +0300

    ...

--- scripts/check-commit.sh
+++ scripts/check-commit.sh
@@ -14,16 +14,16 @@ CRUD_EDITOR_COMMIT=$(git log -1 --format=format:%H --full-diff crud-editor)
 # latest commit where task-manager was changed
 TASK_MANAGER_COMMIT=$(git log -1 --format=format:%H --full-diff task-manager)
 
-REPO="fsm-workflow"
-CIRCLE_CI_TOKEN="whatever"
-
 # replace "/", "#", etc. in current git branch name
 urlencode() {
   node -e "console.log('${*}'.replace('/', '%2F').replace('#', '%23'))"
 }
 
+REPO="fsm-workflow"
+CIRCLE_CI_TOKEN="whatever"
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 SAFE_GIT_BRANCH=`urlencode $GIT_BRANCH`
+
 echo "Current branch is $SAFE_GIT_BRANCH"
 
 if [ $CORE_COMMIT = $LATEST_COMMIT ]
@@ -35,7 +35,7 @@ fi
 if [ $CRUD_EDITOR_COMMIT = $LATEST_COMMIT ]
   then
     echo "files in crud-editor has changed"
-    JOB_URL=`curl -s -X POST -d build_parameters[CIRCLE_JOB]=$3 https://circleci.com/api/v1.1/project/github/opuscapita/${REPO}/tree/${SAFE_GIT_BRANCH}?circle-token=${CIRCLE_CI_TOKEN} | grep build_url | perl -pe 's/^.*(?=https)//g' | perl -pe 's/".*$//g'`
+    JOB_URL=`curl -s -X POST -d build_parameters[CIRCLE_JOB]=build-crud-editor https://circleci.com/api/v1.1/project/github/opuscapita/${REPO}/tree/${SAFE_GIT_BRANCH}?circle-token=${CIRCLE_CI_TOKEN} | grep build_url | perl -pe 's/^.*(?=https)//g' | perl -pe 's/".*$//g'`
     echo $JOB_URL
 fi
 
