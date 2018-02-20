c017a2c3baab9f612e0941c848d268f8907e7afe
Egor Stambakio <stambakio@scand.com>
2018-02-20 13:53:39 +0300

    Docker builds correctly.

--- .circleci/scripts/env.js
+++ .circleci/scripts/env.js
@@ -5,7 +5,7 @@ let env = process.env;
 let DOCKER_TAG = env.CIRCLE_BRANCH.replace('/', '-').replace('#', '-');
 
 module.exports = {
-  AZURE_APP_NAME: "opuscapita-filemanager-demo",
+  AZURE_APP_NAME: "opuscapita-fsm-workflow-demo",
   AZURE_APP_SERVICE_PLAN: env.MINSK_CORE_DEMO_AZURE_APP_SERVICE_PLAN,
   AZURE_PASS: env.AZURE_PASS,
   AZURE_RESOURCE_GROUP: env.MINSK_CORE_DEMO_AZURE_RESOURCE_GROUP,
@@ -21,8 +21,8 @@ module.exports = {
   PROJECT_REPONAME: env.CIRCLE_PROJECT_REPONAME,
   PROJECT_USERNAME: env.CIRCLE_PROJECT_USERNAME,
 
-  API_KEY: env.MINSK_CORE_DEMO_GOOGLE_DRIVE_API_KEY,
-  CLIENT_ID: env.MINSK_CORE_DEMO_GOOGLE_DRIVE_CLIENT_ID,
+  // API_KEY: env.MINSK_CORE_DEMO_GOOGLE_DRIVE_API_KEY,
+  // CLIENT_ID: env.MINSK_CORE_DEMO_GOOGLE_DRIVE_CLIENT_ID,
   CHANGELOG_GITHUB_TOKEN: env.MINSK_CORE_DEMO_CHANGELOG_GITHUB_TOKEN,
-  SERVER_URL: `http://opuscapita-filemanager-demo-${DOCKER_TAG}.azurewebsites.net`
+  SERVER_URL: `http://opuscapita-fsm-workflow-demo-${DOCKER_TAG}.azurewebsites.net`
 };
