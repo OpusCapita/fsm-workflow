3314162409779bce82047d48400d20f98b9b353e
Dmitry Shienok <dshienok@scand.com>
2018-02-26 11:17:45 +0300

    Updated release script. [ci skip]

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -36,6 +36,7 @@ node_version() {
     find . -type f -name "package.json" -exec sed -i 's|\"@opuscapita/fsm-workflow-core\".*\"|\"@opuscapita/fsm-workflow-core\": \"'$1'\"|g' {} \;
     find . -type f -name "package.json" -exec sed -i 's|\"@opuscapita/fsm-workflow-task-manager\".*\"|\"@opuscapita/fsm-workflow-task-manager\": \"'$1'\"|g' {} \;
     find . -type f -name "package.json" -exec sed -i 's|\"@opuscapita/fsm-workflow-history\".*\"|\"@opuscapita/fsm-workflow-history\": \"'$1'\"|g' {} \;
+    find . -type f -name "package.json" -exec sed -i 's|\"@opuscapita/fsm-workflow-editor\".*\"|\"@opuscapita/fsm-workflow-editor\": \"'$1'\"|g' {} \;
     git --no-pager diff --color
 }
 
