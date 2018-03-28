b0f1f2d2b0a25b2feaf93a7b7d91bde1814ee00f
Egor Stambakio <stambakio@scand.com>
2018-03-28 15:02:26 +0300

    Fixing gh-pages build.

--- packages/editor/scripts/gh-pages/build.sh
+++ packages/editor/scripts/gh-pages/build.sh
@@ -2,5 +2,4 @@
 
 rm -rf .gh-pages-tmp &&
 node node_modules/@opuscapita/react-showroom-server/src/bin/showroom-scan.js src &&
-node node_modules/webpack/bin/webpack.js --config config/webpack.config.showroom.js &&
-cp ./www/index.html .gh-pages-tmp
+node node_modules/webpack/bin/webpack.js --config config/webpack.config.showroom.js
