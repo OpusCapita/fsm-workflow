aaf5c7b85006ad8e28e918c6a3256100920dd975
Egor Stambakio <stambakio@scand.com>
2018-04-04 09:56:18 +0300

    Fixing editor gh-pages deploy script.

--- packages/editor/scripts/gh-pages/deploy.sh
+++ packages/editor/scripts/gh-pages/deploy.sh
@@ -30,11 +30,8 @@ urlencode() {
 SAFE_GIT_BRANCH=`urlencode $GIT_BRANCH`
 echo "Current branch is $SAFE_GIT_BRANCH"
 
-GH_EMAIL="stambakio@scand.com"
-GH_NAME="Egor Stambakio"
-
 # now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.email "$GH_MAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
 # switch into the the gh-pages branch
