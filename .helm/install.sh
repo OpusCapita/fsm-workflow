70dc485cf04ef3a0b03209f97b84258e836764b4
Egor Stambakio <stambakio@scand.com>
2018-08-20 13:53:40 +0300

    Add ingress host value

--- .helm/install.sh
+++ .helm/install.sh
@@ -11,6 +11,7 @@ helm upgrade \
   --force \
   --set image.repository="${DOCKER_REPO}" \
   --set image.tag="${DOCKER_TAG}" \
+  --set ingress.host="${MINSK_CORE_K8S_HOST}" \
   --set ingress.baseUrl="${BASE_URL}" \
   --set github.project="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}" \
   --set github.user="${GH_NAME}" \
