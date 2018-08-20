4c9ea035864390c15b4dd43cdbe635ff3bfcc58c
Egor Stambakio <stambakio@scand.com>
2018-08-20 15:39:45 +0300

    ...

--- .circleci/scripts/deploy-docker-image.sh
+++ .circleci/scripts/deploy-docker-image.sh
@@ -2,6 +2,6 @@
 
 . .circleci/scripts/common.sh
 
-docker build -f Dockerfile.demo -t ${DOCKER_REPO}:${DOCKER_TAG} .
+docker build -f Dockerfile.demo -t ${DOCKER_REPO}:${DOCKER_TAG} --build-arg BASE_URL=$BASE_URL .
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 docker push ${DOCKER_REPO}:${DOCKER_TAG}
--- packages/examples/complete-demo/start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-npm run demo:build
-npm run demo:start
