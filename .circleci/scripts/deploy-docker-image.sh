9880b7b8e5fb9c90392b9d11e75c8bc3d4c1d5d5
Egor Stambakio <stambakio@scand.com>
2018-08-22 14:21:10 +0300

    Remove obsolete args piping

--- .circleci/scripts/deploy-docker-image.sh
+++ .circleci/scripts/deploy-docker-image.sh
@@ -2,6 +2,6 @@
 
 . .circleci/scripts/common.sh
 
-docker build -f Dockerfile.demo -t ${DOCKER_REPO}:${DOCKER_TAG} --build-arg BASE_URL=$BASE_URL .
+docker build -f Dockerfile.demo -t ${DOCKER_REPO}:${DOCKER_TAG} .
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 docker push ${DOCKER_REPO}:${DOCKER_TAG}
