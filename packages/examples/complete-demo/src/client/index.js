fa9d9a9959eb32dce4687d3b7cc1dcd8a0a269ae
Egor Stambakio <stambakio@scand.com>
2018-08-22 13:36:33 +0300

    ...

--- packages/examples/complete-demo/src/client/index.js
+++ packages/examples/complete-demo/src/client/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.react';
 
-console.log({ App });
-
 ReactDOM.render(<App />, document.getElementById('main'));
