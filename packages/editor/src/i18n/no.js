6b403c3a25ab5fac1dfa5779a9466d4fdae70dab
Egor Stambakio <stambakio@scand.com>
2018-09-28 11:53:14 +0300

    Add i18n templates for other langs.

--- packages/editor/src/i18n/no.js
+++ packages/editor/src/i18n/no.js
@@ -197,10 +197,10 @@ export const preview = {
 }
 
 export const select = {
-  typeToSearch: `Geben Sie etwas ein, um zu suchen`,
-  createOption: `Eine Option "{option}" anlegen`,
-  clearValue: `Inhalt löschen`,
-  clearAll: `Alle löschen`,
-  nothingFound: `Keine Treffer gefunden`,
-  loading: `Laden…`
+  typeToSearch: `Type to search`,
+  createOption: `Create option "{option}"`,
+  clearValue: `Clear value`,
+  clearAll: `Clear all`,
+  nothingFound: `No results found`,
+  loading: `Loading…`
 }
