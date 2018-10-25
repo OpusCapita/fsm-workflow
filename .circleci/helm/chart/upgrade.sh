a8d0b67bb1a8e906678904862a9dec740ee89d51
Alexey Sergeev <asergeev-sc@users.noreply.github.com>
2018-10-26 00:10:08 +0300

    CI&CD configuration refactoring (#85)

--- .circleci/helm/chart/upgrade.sh
+++ .circleci/helm/chart/upgrade.sh
@@ -2,46 +2,68 @@
 
 set -e
 
-. ../../scripts/common.sh
+# ToDo write down exactly what this slugify does!!!!!!!!
+slugify() {
+  echo $1 | iconv -t ascii//TRANSLIT | sed -E s/[^a-zA-Z0-9]+/-/g | sed -E s/^-+\|-+$//g | tr A-Z a-z | cut -c1-53
+}
 
-RELEASE_NAME=$(slugify "${CIRCLE_PROJECT_REPONAME}-${CIRCLE_BRANCH}")
-PROJECT="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
-DEPLOYMENT_URL="http://${MINSK_CORE_K8S_HOST}${BASE_URL}"
+GITHUB_PROJECT="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
 
-/bin/scripts/login/aks-login.sh ${MINSK_CORE_K8S_AZURE_RG} ${MINSK_CORE_K8S_AZURE_NAME}
+DOCKER_IMAGE_REPOSITORY=$(echo ${GITHUB_PROJECT} | tr '[:upper:]' '[:lower:]')-demo
+DOCKER_IMAGE_TAG=$(slugify "${CIRCLE_BRANCH}")
 
+APPLICATION_URL_PATH=/${CIRCLE_PROJECT_REPONAME}/$(slugify $CIRCLE_BRANCH)
+APPLICATION_URL="http://${MINSK_CORE_K8S_DEMO_DEPLOYMENTS_HOST}${APPLICATION_URL_PATH}"
+
+echo "GITHUB_PROJECT: '${GITHUB_PROJECT}'"
+echo "DOCKER_IMAGE_REPOSITORY: '${DOCKER_IMAGE_REPOSITORY}'"
+echo "DOCKER_IMAGE_TAG: '${DOCKER_IMAGE_TAG}'"
+echo "APPLICATION_URL_PATH: '${APPLICATION_URL_PATH}'"
+echo "APPLICATION_URL: '${APPLICATION_URL}'"
+
+# login to azure
+az login -u "$AZURE_USER" -p "$AZURE_PASS" &> /tmp/az-login.log
+# use Minsk Core subscription
+az account set -s "$MINSK_CORE_AZURE_SUBSCRIPTION_ID"
+# access Minsk Core cluster
+az aks get-credentials -n "$MINSK_CORE_K8S_AZURE_NAME" -g "$MINSK_CORE_K8S_AZURE_RG"
+# configure $HELM_HOME
 helm init --client-only
 
-helm dependency build
+# put Helm dependencies into correct folder
+helm dependency update
 
+# install/update release
 helm upgrade \
   --install \
   --force \
   --set ingress.host="${MINSK_CORE_K8S_HOST}" \
-  --set image.repository="${IMAGE_REPOSITORY}" \
-  --set image.tag="${IMAGE_TAG}" \
-  --set ingress.baseUrl="${BASE_URL}" \
+  --set ingress.path="${APPLICATION_URL_PATH}"\
+  \
+  --set image.repository="${DOCKER_IMAGE_REPOSITORY}" \
+  --set image.tag="${DOCKER_IMAGE_TAG}" \
   \
   --set github-status-deployment-link.github.user="${GH_NAME}" \
   --set github-status-deployment-link.github.password="${GH_PASS}" \
-  --set github-status-deployment-link.github.project="${PROJECT}" \
+  --set github-status-deployment-link.github.project="${GITHUB_PROJECT}" \
   --set github-status-deployment-link.github.ref="${CIRCLE_SHA1}" \
-  --set github-status-deployment-link.url="${DEPLOYMENT_URL}" \
+  --set github-status-deployment-link.url="${APPLICATION_URL}" \
   \
   --set selfkiller.azureAks.resourceGroup="${MINSK_CORE_K8S_AZURE_RG}" \
   --set selfkiller.azureAks.clusterName="${MINSK_CORE_K8S_AZURE_NAME}" \
-  --set selfkiller.image.repository="${IMAGE_REPOSITORY}" \
-  --set selfkiller.image.tag="${IMAGE_TAG}" \
-  --set selfkiller.github.project="${PROJECT}" \
+  --set selfkiller.image.repository="${DOCKER_IMAGE_REPOSITORY}" \
+  --set selfkiller.image.tag="${DOCKER_IMAGE_TAG}" \
+  --set selfkiller.github.project="${GITHUB_PROJECT}" \
   --set selfkiller.github.branch="${CIRCLE_BRANCH}" \
   \
   --set slack-notifications.webhook="${MINSK_CORE_SLACK_CI_WEBHOOK_URL}" \
-  --set slack-notifications.github.project="${PROJECT}" \
+  --set slack-notifications.github.project="${GITHUB_PROJECT}" \
   --set slack-notifications.github.branch="${CIRCLE_BRANCH}" \
   --set slack-notifications.github.user="${CIRCLE_USERNAME}" \
   --set slack-notifications.github.ref="${CIRCLE_SHA1}" \
   --set slack-notifications.ci.jobUrl="${CIRCLE_BUILD_URL}" \
-  --set slack-notifications.link.url="${DEPLOYMENT_URL}" \
+  --set slack-notifications.link.url="${APPLICATION_URL}" \
   \
   --namespace "${MINSK_CORE_K8S_NAMESPACE_DEVELOPMENT}" \
-  ${RELEASE_NAME} .
+  $(slugify "${CIRCLE_PROJECT_REPONAME}-${CIRCLE_BRANCH}") \
+  .
---
+++ .circleci/scripts/build-and-push.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+slugify() {
+  # replaces non latin characters to latin
+  # replaces all not letters/numbers with '-'
+  # replaces all '-' at the beginning and at the end
+  # replaces all upper case letters with lower case letters
+  # returns substring using first 53 letter
+  echo $1 | iconv -t ascii//TRANSLIT | sed -E s/[^a-zA-Z0-9]+/-/g | sed -E s/^-+\|-+$//g | tr A-Z a-z | cut -c1-53
+}
+
+GITHUB_PROJECT="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
+
+DOCKER_IMAGE_REPOSITORY=$(echo ${GITHUB_PROJECT} | tr '[:upper:]' '[:lower:]')-demo
+DOCKER_IMAGE_TAG=$(slugify "${CIRCLE_BRANCH}")
+
+docker login -u $DOCKER_USER -p $DOCKER_PASS && \
+docker build -f Dockerfile.demo --no-cache \
+       -t $DOCKER_IMAGE_REPOSITORY:$DOCKER_IMAGE_TAG . && \
+docker push $DOCKER_IMAGE_REPOSITORY:$DOCKER_IMAGE_TAG
