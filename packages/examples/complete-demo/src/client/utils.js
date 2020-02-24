e2dc9502cd96caaf1d8a6db5247d5692d6171260
Egor Stambakio <egor.stambakio@opuscapita.com>
2020-02-24 15:59:16 +0300

    log baseURI

--- packages/examples/complete-demo/src/client/utils.js
+++ packages/examples/complete-demo/src/client/utils.js
@@ -1,3 +1,5 @@
 export const baseUrl = (document.baseURI || '/').replace(new RegExp(`\.+${window.location.host}`), '');
 
 export const url = path => `${baseUrl}${path}`.replace(/\/{2,}/, '/');
+
+console.log({ baseUrl })
